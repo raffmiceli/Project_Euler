4e929e903aff1d0abeff2a9023e0ca90d6ae09d1
raffmiceli <42178501+raffmiceli@users.noreply.github.com>
2019-07-20 00:38:13 -0400

    Update primeList.py

--- primeList.py
+++ primeList.py
@@ -10,3 +10,16 @@ def primeList(e):
         else: primes.append(x)
         x += 1
     return primes
+
+# This could be improved by breaking out of the inner for loop when p exceeds sqrt(x), like so:
+def primeList2(e):
+    primes = [2]
+    x = 3
+    while x <= e:
+        for p in primes:
+            if not x % p: break
+            if p > x**.5:
+                primes.append(p)
+                break
+        x += 1
+    return primes
