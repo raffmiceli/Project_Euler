2fd728774cf8bb91c111fdca55b4efb62dedace8
raffmiceli <42178501+raffmiceli@users.noreply.github.com>
2019-07-20 19:04:43 -0400

    Update Problem 5.py

--- Problem 5.py
+++ Problem 5.py
@@ -1,3 +1,11 @@
+# Problem 5 from Project Euler (projecteuler.net)
+
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+# This one's actually pretty doable by hand. Just list all the positive integers less than 20, work out their prime factorizations,
+# then multiply together the primes less than 20, each raised to the highest power found in the factorizations.
 ##1 = 1
 ##2 = 2
 ##3 = 3
@@ -24,12 +32,14 @@
 from primeList import *
 from pyprimes import *
 
+# Function to find the highest power necessary for an integer.
 def numPowers(x,n):
     y = 1
     while x**(y+1) <= n:
         y += 1
     return y
 
+# Multiplies primes raised to correct power.
 def lrgstNumDiv(n):
     t = 1
     for x in list(primes_below(n)):
@@ -37,3 +47,12 @@ def lrgstNumDiv(n):
     return t
 
 print lrgstNumDiv(20)
+
+# There's a nice pythonic one-liner way to do this using logarithms and reduce:
+from math import floor, log
+from operator import mul
+from functools import reduce
+from pyprimes import primes_below
+
+print(reduce(mul,[p**floor(log(20,p)) for p in primes_below(20)]))
+
