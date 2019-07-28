50ac3682b7a1192ff4ddb0eefe6c6e01464280f6
raffmiceli <42178501+raffmiceli@users.noreply.github.com>
2019-07-28 13:59:45 -0400

    Update and rename Problem 10.py to Problem 010.py

--- Problem 010.py
+++ Problem 010.py
@@ -1,3 +1,10 @@
+# Problem 10 from Project Euler (projecteuler.net)
+
+# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+# Find the sum of all the primes below two million.
+
+# Same prime list generator from earlier problem(s), just added an accumulator in each step.
 def primeSum1(e):
     primes = [2]
     x = 3
@@ -8,7 +15,7 @@ def primeSum1(e):
         else:
             primes.append(x)
             t += x
-            #print x, t
+            #print(x, t)
         x += 2
     return t
 
@@ -17,22 +24,26 @@ def primeSum1(e):
 from eraSieve import *
 from pyprimes import *
 
+# Used my own Sieve of Eratosthenes but then switched to the primes_below() function from the PyPrimes library.
 def primeSum2(x):
     t = 0
     #primes = eraSieve(x)
     primes = list(primes_below(x))
-    print 'List of primes complete.'
+    print('List of primes complete.')
     for prime in primes:
         t += prime
         #print prime, t
     return t
 
+# Same as before but switched from summing through a for loop to just using the sum() method
 def primeSum3(x):
     #primes = eraSieve(x)
     primes = list(primes_below(x))
-    print 'List of primes complete.'
-    print sum(primes)
+    print('List of primes complete.')
+    print(sum(primes))
 
+# Implemented my own sieve of Eratosthenes with summation each time a prime is found. Pretty inefficient since
+# it doesn't jump to the next prime, just increments by 2.
 def primeSum4(n):
     marked = [0] * n
     value = 3
@@ -47,6 +58,8 @@ def primeSum4(n):
         value += 2
     return s
 
+# Not sure what I was doing with this one. Just seems like a copy of my first method but I replaced the step-by-step summation with
+# a sum() function at the end.
 def primeSum5(e):
     primes = [2]
     x = 3
@@ -61,13 +74,14 @@ def primeSum5(e):
         else:
             primes.append(x)
             #t += x
-            #print x, t
+            #print(x, t)
         x += 2
     #return t
     return sum(primes)
 
-print primeSum5(2000000)
+print(primeSum5(2000000))
 
+# Just a more condensed sieve implementation.
 def sieve_prime(n):                                                                  
     index=list(range(n))                                                             
     for i in range(2,int(n**0.5)+1):                                                 
@@ -76,4 +90,4 @@ def sieve_prime(n):
                 index[m]=0                                                           
     return [p for p in index if p][1:]                                               
                                                                                      
-#print sum(sieve_prime(2000000))
+#print(sum(sieve_prime(2000000)))
