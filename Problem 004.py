2ca28f4777e45e7783ad8c1f822b30e630976c9b
raffmiceli <42178501+raffmiceli@users.noreply.github.com>
2019-07-28 01:11:52 -0400

    Update Problem 004.py

--- Problem 004.py
+++ Problem 004.py
@@ -15,9 +15,9 @@ for m in range(900,1000):
     for n in range(900,1000):
         mn = m*n
         if str(mn) == str(mn)[::-1]:
-            #print m, n, mn
+            #print(m, n, mn)
             if mn >= h: h = mn
-print h
+print(h)
 
 # Pythonic one-liner solution
-print max([x*y for x in range(900,1000) for y in range(900,1000) if str(x*y) == str(x*y)[::-1]])
+print(max([x*y for x in range(900,1000) for y in range(900,1000) if str(x*y) == str(x*y)[::-1]]))
